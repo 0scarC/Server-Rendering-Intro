982fcad6d5bec0a02d80b3a3417411a52e1dd653
Oscar <admin@Admins-MacBook-Pro-2.local>
2022-06-07 20:14:13 -0500

    Code Along 2 done

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -2,10 +2,18 @@ const express = require(`express`)
 const breads = express.Router()
 const Bread = require(`../models/bread.js`)
 
+//INDEX
 breads.get(`/`, function(req, res) {
+    res.render(`Index`, 
+        {
+            breads: Bread,
+            title: `Index Page`
+        }
+    )
     res.send(`This is index at /breads`)
 })
 
+//SHOW
 breads.get(`/:arrayIndex`, function(req, res) {
     res.send(Bread[req.params.arrayIndex])
 })
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+//DEPENDENCIES
 const express = require(`express`)
 
+//CONFIGURATION
 require(`dotenv`).config()
 const PORT = process.env.PORT
 const app = express()
 console.log(PORT)
 
+//ROUTES
 app.get(`/`, function(req, res) {
     res.send(`Welcome to an Awesome App about Breads!`)
 })
 
+//BREADS
+const breadsController = require(`./controllers/bread_controller.js`)
+app.use(`/breads`, breadsController)
+
+//MIDDLEWARE
+app.set(`views`, __dirname + `/views`)
+app.set(`view engine`, `jsx`)
+app.engine(`jsx`, require(`express-react-views`).createEngine())
+
+//LISTEN
 app.listen(PORT, function() {
     console.log(`nomming at port`, PORT)
 })
-
-const breadsController = require(`./controllers/bread_controller.js`)
-app.use(`/breads`, breadsController)
